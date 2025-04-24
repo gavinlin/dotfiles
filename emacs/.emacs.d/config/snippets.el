cdaa80439b663d09d48610d38703caf25c3a20da
Gavin <gavinlin.au@gmail.com>
2025-04-24 16:35:34 +1000

    fix snippets

--- emacs/.emacs.d/config/snippets.el
+++ emacs/.emacs.d/config/snippets.el
@@ -7,15 +7,6 @@
 (use-package yasnippet-snippets
   :after yasnippet)
 
-;; Enable yasnippet in text/org modes too
-(dolist (hook '(prog-mode-hook text-mode-hook org-mode-hook))
-  (add-hook hook #'yas-minor-mode))
-
-;; Optional: set up snippet directory
-(setq yas-snippet-dirs
-      '("~/.emacs.d/snippets"
-        yasnippet-snippets-dir))
-
 ;; Optional: enable yas as fallback if corfu/cape miss
 ;; Avoid broken CAPF setup by NOT inserting yasnippet-capf directly
 
